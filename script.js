0deb7555e25520dd23eb4eea41b15f88d21cd8f8
Cirill <florenzdalangin@gmail.com>
2020-09-19 14:02:54 +0000

    useless and barbee

--- script.js
+++ script.js
@@ -17,4 +17,36 @@ function change() {
         document.getElementById("titleA").style.display = "none";
         return a = 1;
     }
+};
+
+var count = 0;
+function useless() {
+  if (count == 0) {
+    alert("What did you expect?")
+    console.log(count)
+  }
+  count++
+  if (count == 1) {
+    alert("Why did you do this?");
+  }
+  count++
+  if (count == 2) {
+    alert("See you wouldn't be stuck in this loop if you didn't push the button.");
+  }
+  count++
+  if (count == 3) {
+    alert("Dis u: 🤡");
+  }
+  count++
+  if (count == 4) {
+    alert("I hope you learned your lesson");
+  }
+  count++
+  if (count == 5) {
+    alert("Stop going around pressing random buttons");
+  }
+  count++
+  if (count == 6) {
+    alert("Have a great day");
+  }
 }
