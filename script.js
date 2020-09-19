3131cc5b0adb8b734b62b96a36270c409f5b6012
Cirill <florenzdalangin@gmail.com>
2020-09-19 04:49:59 +0000

    perry and mask pages

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 var a;
-function switch(){
-  if(a==0) {
-    document.getElementById("ptp").style.display = "inline";
-    // document.getElementById("ptp2").style.display = "none";
-    return a=0;
-  }
-  else {
-  document.getElementById("ptp").style.display = "none";
-  // document.getElementById("ptp2").style.display = "inline";
-  return a=1;
+
+function change() {
+    if (a == 0) {
+        document.getElementById("ptp").style.display = "initial";
+        document.getElementById("ptp2").style.display = "none";
+
+        document.getElementById("titleA").style.display = "initial";
+        document.getElementById("titleB").style.display = "none";
+        return a = 0;
+    }
+    else {
+        document.getElementById("ptp").style.display = "none";
+        document.getElementById("ptp2").style.display = "initial";
+
+        document.getElementById("titleB").style.display = "initial";
+        document.getElementById("titleA").style.display = "none";
+        return a = 1;
+    }
 }
